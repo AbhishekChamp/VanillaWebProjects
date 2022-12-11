5da6cbf006c03da345c9468912c91af6885489c7
AbhishekChamp <abhishek96.r@gmail.com>
2022-12-11 18:17:57 +0530

    Project-14 Part-2

--- 14_Speech_Text_Reader/script.js
+++ 14_Speech_Text_Reader/script.js
@@ -70,5 +70,74 @@ function createBox(item) {
         <p class="info">${text}</p>
     `;
 
+    box.addEventListener("click", () => {
+        setTextMessage(text);
+        speakText();
+
+        // Add active effect
+        box.classList.add("active");
+        setTimeout(() => {
+            box.classList.remove("active");
+        }, 800);
+    });
+
     main.appendChild(box);
 }
+
+// Init speech synthesis
+const message = new SpeechSynthesisUtterance();
+
+// Store Voices
+let voices = [];
+
+function getVoices() {
+    voices = speechSynthesis.getVoices();
+
+    voices.forEach((voice) => {
+        const option = document.createElement("option");
+
+        option.value = voice.name;
+        option.innerText = `${voice.name} ${voice.lang}`;
+
+        voicesSelect.appendChild(option);
+    });
+}
+
+// Set text
+function setTextMessage(text) {
+    message.text = text;
+}
+
+// Speak Text
+function speakText() {
+    speechSynthesis.speak(message);
+}
+
+// Set Voice
+function setVoice(e) {
+    message.voice = voices.find((voice) => voice.name === e.target.value);
+}
+
+// Voices Changed
+speechSynthesis.addEventListener("voiceschanged", getVoices);
+
+// Toggle text box
+toggleBtn.addEventListener("click", () =>
+    document.getElementById("text-box").classList.toggle("show"),
+);
+
+// Close button
+closeBtn.addEventListener("click", () =>
+    document.getElementById("text-box").classList.remove("show"),
+);
+
+// Change Voice
+voicesSelect.addEventListener("change", setVoice);
+
+// Read text button
+readBtn.addEventListener("click", () => {
+    setTextMessage(textarea.value);
+    speakText();
+});
+
+getVoices();
